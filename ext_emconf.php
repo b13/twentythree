e3167671c40c5ee19b36b80be8089af39ffb0f24
Oliver Bartsch <bo@cedev.de>
2025-08-13 16:52:17 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'b13 GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '0.3.1',
+    'version' => '0.3.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '11.5.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => []
